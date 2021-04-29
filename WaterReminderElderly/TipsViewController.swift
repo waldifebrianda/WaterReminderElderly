495bd5989de50e624040a8656b8e66c2f28af1f0
Waldi Febrianda <waldifebrianda@gmail.com>
2021-04-29 15:08:39 +0700

    -testing

--- WaterReminderElderly/TipsViewController.swift
+++ WaterReminderElderly/TipsViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class TipsViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+    
         //title = "Tips"
     }
 }
